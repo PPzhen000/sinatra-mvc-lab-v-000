311d89926aaa96d885b215adebf0ddd2d57ac0ef
Ariel Zhen <zhenpingping@live.com>
2018-08-14 20:52:39 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+
+  def initialize(text)
+    @pigtext = piglatinize_sentense(text)
+  end
+
+  def vowel?(word)
+    word.match(/AEIOUaeiou/)
+  end
+
+  def consonant?(word)
+    !vowel?(word)
+  end
+
+  def piglatinize(word)
+    if vowel?(word[0])
+      word = word + "way"
+    elsif consonant?(word[0]) && vowel?(word[1])
+      word = word.slice(1..-1) + word.slice(0) + "ay"
+    elsif consonant?(word[0]) && consonant?(word[1]) && vowel?(word[2])
+        word = word.slice(2..-1) + word.slice(0, 2) + "ay"
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2]) && vowel?(word[3])
+        word = word.slice(3..-1) + word.slice(0, 3) + "ay"
+    end
+    word
+
+  end
+
+  def piglatinize_sentense(sentence)
+    sentence = sentence.split(" ")
+    sentence = sentence.collect {|word| piglatinize(word)}
+    sentence = sentence.join(" ")
+  end
+end
